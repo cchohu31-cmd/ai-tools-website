ff9cb6de2cb37e9a50f4f7ecaf373b03eded938e
cchohu31-cmd <cchohu31@gmail.com>
2026-07-01 13:05:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getFirestore,
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCL4AaIpTwhdehdAg83ZBNO0QxaFVZhhWQ",
+  authDomain: "ai-tools-website-7d277.firebaseapp.com",
+  projectId: "ai-tools-website-7d277",
+  storageBucket: "ai-tools-website-7d277.firebasestorage.app",
+  messagingSenderId: "351773858776",
+  appId: "1:351773858776:web:6fc7a597bbcee3200930ad"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 // Sample AI Tools Data
 const aiTools = [
     { name: "ChatGPT", category: "text", desc: "Advanced conversational AI for writing, coding, and analysis.", icon: "fa-robot" },
